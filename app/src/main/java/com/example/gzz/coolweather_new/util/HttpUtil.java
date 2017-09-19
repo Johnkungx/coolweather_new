2b9cd5e52d039ae93e3c630dc4f3a9dc34b67588
John <243376479@qq.com>
2017-09-19 10:26:01 +0800

    加入表的获取和表的布布局

---
+++ app/src/main/java/com/example/gzz/coolweather_new/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.gzz.coolweather_new.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by gzz on 2017/9/18.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequset(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
